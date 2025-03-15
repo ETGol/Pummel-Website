29f602af40327a8a409f55a32e614a3bb41548cb
Ethan Goldean <126651808+ETGol@users.noreply.github.com>
2025-03-14 19:58:26 -0500

    Add files via upload

---
+++ pummelScript.js
@@ -0,0 +1,9 @@
+function test(){
+    var r1 = document.getElementById("rank-1").value;
+    var r2 = document.getElementById("rank-2").value;
+    var r3 = document.getElementById("rank-3").value;
+ 
+   
+    alert(r1 + " " + r2 + " " + r3);
+
+}
